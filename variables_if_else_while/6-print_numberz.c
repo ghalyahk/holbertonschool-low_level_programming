2b2b7239317cc9437073747177ed10be55b6be6d
ghalyahk <ghalyah1166@gmail.com>
2025-08-30 17:09:26 +0000

    Fixed indentation and formatting to comply with Betty style

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - Prints numbers from 0 to 9
+ *
+ * Description: This program prints all digits of base 10 starting from 0
+ * and ends with a new line. It uses only the putchar function and no
+ * variables of type char.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + '0');  /* تحويل الرقم إلى الحرف المناسب في ASCII */
-    }
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
 
-    putchar('\n');  /* طباعة سطر جديد في النهاية */
-    return (0);
+	putchar('\n');
+	return (0);
 }
