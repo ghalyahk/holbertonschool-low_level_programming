dc41b93220c3453b6b95fb9f4aaac3d5b42c7915
ghalyahk <ghalyah1166@gmail.com>
2025-08-30 17:45:35 +0000

    Fix tab and indentation issues for Betty style

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,31 +2,43 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: Prints the last digit of a random number and
+ * evaluates if the last digit is greater than 5, 0, or less than 6 and not 0.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int n;
-    int last_digit;
+	int n;
+	int last_digit;
 
-    srand(time(0));  /* Initialize the random number generator */
-    n = rand() - RAND_MAX / 2;  /* Generate a random number */
-    last_digit = n % 10;  /* Get the last digit of the number */
+	/* Initialize random number generator */
+	srand(time(0));
 
-    /* Print the result based on the value of the last digit */
-    printf("Last digit of %d is %d and is ", n, last_digit);
+	/* Generate a random number */
+	n = rand() - RAND_MAX / 2;
 
-    if (last_digit > 5)
-    {
-        printf("greater than 5\n");
-    }
-    else if (last_digit == 0)
-    {
-        printf("0\n");
-    }
-    else
-    {
-        printf("less than 6 and not 0\n");
-    }
+	/* Get the last digit of n */
+	last_digit = n % 10;
 
-    return (0);
+	/* Print the result based on the value of the last digit */
+	printf("Last digit of %d is %d and is ", n, last_digit);
+
+	if (last_digit > 5)
+	{
+		printf("greater than 5\n");
+	}
+	else if (last_digit == 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		printf("less than 6 and not 0\n");
+	}
+
+	return (0);
 }
