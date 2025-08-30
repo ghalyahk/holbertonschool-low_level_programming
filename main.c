48ffa188681b3e0151ae0b3c5c1b175dcb2b3a31
ghalyahk <ghalyah1166@gmail.com>
2025-08-30 13:41:53 +0000

    Add solution for 0-positive_or_negative

--- main.c
+++ main.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    return (0);
+    int n;
+
+    /* تهيئة مولد الأرقام العشوائية */
+    srand(time(0)); 
+
+    /* توليد رقم عشوائي */
+    n = rand() - RAND_MAX / 2;
+
+    /* التحقق إذا كان الرقم موجبًا أو سالبًا أو صفر */
+    if (n > 0)
+    {
+        printf("%d is positive\n", n);
+    }
+    else if (n == 0)
+    {
+        printf("%d is zero\n", n);
+    }
+    else
+    {
+        printf("%d is negative\n", n);
+    }
+
+    return 0;
 }
