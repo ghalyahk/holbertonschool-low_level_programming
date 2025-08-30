a0c76224ad16116a12c5d64fdc09f606e4599b8d
ghalyahk <ghalyah1166@gmail.com>
2025-08-30 20:19:41 +0000

    Add times_table solution and main header and main file

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - Prints the 9 times table starting from 0
+ */
+void times_table(void)
+{
+	int i, j, prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			prod = i * j;
+			if (j == 0)
+			{
+				printf("%d", prod);
+			}
+			else
+			{
+				printf(", %2d", prod);
+			}
+		}
+		printf("\n");
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,2 +1,2 @@
-int add(int a, int b);
+void times_table(void);
 
