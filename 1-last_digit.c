eac7737dffbfb3778f50843788e569ce020c023c
ghalyahk <ghalyah1166@gmail.com>
2025-08-30 14:17:55 +0000

    تم إضافة التعديلات على 1-last_digit

--- 1-last_digit.c
+++ 1-last_digit.c
@@ -1,33 +1,35 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
     int n;
     int last_digit;
 
-    srand(time(0));  /* هذا لتوليد أرقام عشوائية مختلفة في كل مرة */
-    n = rand() - RAND_MAX / 2;  /* هذا يجعل الرقم ضمن المجال السالب والموجب */
+    srand(time(0));    // لتوليد أرقام عشوائية باستخدام الوقت
+    n = rand() - RAND_MAX / 2;  // توليد عدد عشوائي بين -RAND_MAX/2 و +RAND_MAX/2
 
-    last_digit = n % 10;  /* الحصول على الرقم الأخير */
+    // الحصول على الرقم الأخير
+    last_digit = n % 10;
 
-    printf("Last digit of %d is %d", n, last_digit);
-
+    // طباعة الرقم الأخير مع التفسير
     if (last_digit > 5)
     {
-        printf(" and is greater than 5");
+        printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
     }
     else if (last_digit == 0)
     {
-        printf(" and is 0");
+        printf("Last digit of %d is %d and is 0\n", n, last_digit);
     }
-    else if (last_digit < 6 && last_digit != 0)
+    else
     {
-        printf(" and is less than 6 and not 0");
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
     }
 
-    printf("\n");
-
-    return 0;
+    return (0);
 }
+
