ec25b4ac11a5378bc560fc6d8748d6799381c4f9
ghalyahk <ghalyah1166@gmail.com>
2025-09-06 05:06:02 +0000

    Fix: Update reset_to_98 to match Betty coding style

--- 0-reset_to_98.c
+++ 0-reset_to_98.c
@@ -3,9 +3,11 @@
 /**
  * reset_to_98 - Takes a pointer to an int and updates
  *               the value it points to to 98.
- * @n: Pointer to an int.
+ * @n: Pointer to an int to be updated.
+ *
+ * Return: Nothing.
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
