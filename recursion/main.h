8758d1fe7a6204eb503299a646564f147bc074fd
ghalyahk <ghalyah1166@gmail.com>
2025-09-13 03:57:17 +0000

    Add _strlen_recursion function with Betty style

--- recursion/main.h
+++ recursion/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _print_rev_recursion(char *s);
-int _putchar(char c);  /* لتستخدم الدالة _putchar */
+int _putchar(char c);
+int _strlen_recursion(char *s);
 
 #endif
