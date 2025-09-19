db5efb549379c0b6921971e504d4f448c176628e
ghalyahk <ghalyah1166@gmail.com>
2025-09-19 23:49:31 +0000

    Fix: add structures_typedef as regular folder, not submodule

--- dog.h
+++ dog.h
@@ -2,18 +2,19 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic info
+ * struct dog - structure for a dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- *
- * Description: Structure to store basic info about a dog.
  */
-struct dog
+typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
-};
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */
