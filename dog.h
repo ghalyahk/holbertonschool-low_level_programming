4c0b4f8e6f5793b5fc29ac7f63bf434f3d1e18d6
ghalyahk <ghalyah1166@gmail.com>
2025-09-20 00:00:58 +0000

    Fixed spacing warnings in dog.h according to Betty style

--- dog.h
+++ dog.h
@@ -2,17 +2,22 @@
 #define DOG_H
 
 /**
- * struct dog - structure for a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * struct dog - Dog struct
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+/**
+ * dog_t - new name for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
