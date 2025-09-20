dc19517d3d3ef74ba6ea31d8a9ec3d6df23d5854
ghalyahk <ghalyah1166@gmail.com>
2025-09-20 00:18:07 +0000

    Fix: add structures_typedef as regular folder, not submodule

---
+++ 4-new_dog.c
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s && s[len])
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strcpy - copies a string manually
+ * @dest: destination buffer
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog and stores a copy of name and owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner's name
+ *
+ * Return: pointer to new dog_t, or NULL if function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int name_len, owner_len;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
+
+	dog->name = malloc(sizeof(char) * (name_len + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * (owner_len + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+}
