dff98dc8664e22d34515d52f50069ae22c534025
ghalyahk <ghalyah1166@gmail.com>
2025-10-10 17:21:05 +0000

    Prepare doubly linked list project

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <fcntl.h>   /* open flags */
+#include <stdlib.h>  /* malloc, free */
+#include <unistd.h>  /* read, write */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd;
+    ssize_t r, w;
+    char *buf;
+
+    if (!filename)
+        return (0);
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
+
+    buf = malloc(sizeof(char) * letters);
+    if (!buf)
+    {
+        close(fd);
+        return (0);
+    }
+
+    r = read(fd, buf, letters);
+    if (r == -1)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
+
+    w = write(STDOUT_FILENO, buf, r);
+    if (w != r)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
+
+    free(buf);
+    close(fd);
+    return (r);
+}
+
