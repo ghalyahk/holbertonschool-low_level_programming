8866fc7ba9a04fdba20415e0c10d6e36b586cccb
ghalyahk <ghalyah1166@gmail.com>
2025-10-10 14:09:18 +0000

    Fix merge conflict in lists.h

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,16 +1,22 @@
-// محتوى lists.h النهائي
 #ifndef LISTS_H
 #define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
+/* تعريف الهيكل للقائمة المزدوجة */
 typedef struct dlistint_s
 {
     int n;
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
+
+/* Prototype للدوال */
+size_t print_dlistint(const dlistint_t *h);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 
 #endif /* LISTS_H */
 
