53f9b885e368e073f6fe5e2f6b3751cf4d8cfd1f
ghalyahk <ghalyah1166@gmail.com>
2025-10-16 16:16:45 +0000

    Add 1-create_file.c, main.h, and README.md

--- file_io/main.h
+++ file_io/main.h
@@ -4,10 +4,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
+/* Prototypes for file I/O functions */
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
 
