5d88eea09076206795b615d753a2146b91639b57
ghalyahk <ghalyah1166@gmail.com>
2025-10-17 14:02:08 +0000

    Add 0-read_textfile.c, main.h, and test file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,24 +1,22 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include "main.h"
+#include <fcntl.h>
+
+#define BUF_SIZE 1024
 
 /**
- * main - copies the content of a file to another file
- * @ac: argument count
- * @av: argument vector
- *
- * Return: 0 on success, exits with code on error
+ * main - copy file content
  */
 int main(int ac, char **av)
 {
     int fd_from, fd_to, r, w;
-    char buffer[1024];
+    char buffer[BUF_SIZE];
 
     if (ac != 3)
     {
-        dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
         exit(97);
     }
 
@@ -29,7 +27,7 @@ int main(int ac, char **av)
         exit(98);
     }
 
-    fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
     if (fd_to == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
@@ -37,10 +35,10 @@ int main(int ac, char **av)
         exit(99);
     }
 
-    while ((r = read(fd_from, buffer, 1024)) > 0)
+    while ((r = read(fd_from, buffer, BUF_SIZE)) > 0)
     {
         w = write(fd_to, buffer, r);
-        if (w == -1 || w != r)
+        if (w != r)
         {
             dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
             close(fd_from);
