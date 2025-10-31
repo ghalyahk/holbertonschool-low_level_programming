ff05466b7c83207ed7bcc6b2d9e6fe9c0d827256
ghalyahk <ghalyah1166@gmail.com>
2025-10-31 20:53:03 +0000

    Merge remote changes

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -4,17 +4,20 @@
  * puts2 - prints every other character of a string
  * @str: pointer to the string
  *
- * Return: nothing
+ * Return: void
  */
 void puts2(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)   /* اطبعي كل حرف في index زوجي */
-            _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
 }
+
