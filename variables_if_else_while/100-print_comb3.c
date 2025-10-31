cbf017d573405bce6fe3010dd49fc13d9d1674a2
ghalyahk <ghalyah1166@gmail.com>
2025-10-31 23:06:40 +0000

    Write a program that prints _putchar, followed by a new line.

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -3,28 +3,29 @@
 /**
  * main - prints all possible different combinations of two digits
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-    int i, j;
+	int i;
+	int j;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = i + 1; j < 10; j++)
-        {
-            putchar(i + '0');
-            putchar(j + '0');
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
 
-            if (!(i == 8 && j == 9))
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+			if (!(i == 8 && j == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
 
