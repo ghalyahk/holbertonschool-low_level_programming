30ee208857e4f5bbfc63eeb641a3103ef8cab4c5
ghalyahk <ghalyah1166@gmail.com>
2025-10-31 23:01:41 +0000

    Write a program that prints _putchar, followed by a new line.

---
+++ variables_if_else_while/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/*
+ * Header for task 100-print_comb3
+ * putchar is available via <stdio.h>, no extra prototypes needed.
+ */
+
+#endif /* MAIN_H */
